70c26cb7f67bed3bde3d627cf16a79d5b3186456
Favor Vincent <vfavorm77@gmail.com>
2024-07-09 14:49:55 +0300

    port table modified

--- server/seed.py
+++ server/seed.py
@@ -8,33 +8,10 @@ from faker import Faker
 
 # Local imports
 from app import app
-from models import db, User,Passenger, Ship, Port, Contractor
+from models import db
+
 
 # Seed data function
-def seed_data():
-    db.create_all()
-    fake = Faker()
-
-    # Create 100 users
-    for _ in range(100):
-        name = fake.name()
-        email = fake.email()
-        password = fake.password()
-        balance = randint(1000, 100000)
-
-        user = User(name=name, email=email, password=password, balance=balance)
-        db.session.add(user)
-
-    # Create 10 ships
-    for _ in range(10):
-        name = fake.company()
-        capacity_weight = randint(1000, 5000)
-        current_weight = 0
-        total_tickets = randint(1000, 5000)
-        available_tickets = total_tickets
-        category = rc(["passenger", "freight"])
-        port_id = randint(1,
-
 if __name__ == '__main__':
     fake = Faker()
     with app.app_context():
