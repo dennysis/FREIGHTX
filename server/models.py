d3ae0dcf3d9d93a29afd8bbe2bc619c6d1a241e8
Kamau-sam <skamau.6411@gmail.com>
2024-07-09 13:30:36 +0300

    Added ship id on passanger model

--- server/models.py
+++ server/models.py
@@ -11,3 +11,11 @@ class User(db.Model, SerializerMixin):
   email = db.Column(db.String(100), unique=True, nullable=False)
   password = db.Column(db.String, nullable=False)
   balance = db.Column(db.Integer, nullable=False)
+
+class Passenger(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    ticket_number = db.Column(db.String(10), unique=True, nullable=False)
+    cost = db.Column(db.Float, nullable=False)
+    destination = db.Column(db.String(100), nullable=False)
+    ship_id = db.Column(db.Integer, db.ForeignKey('ship.id'), nullable=True)
