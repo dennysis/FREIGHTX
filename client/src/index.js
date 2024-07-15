8f1c23a9831fca31315860a5f89a6f0c4fbad13c
diana <gududiana@gmail.com>
2024-07-15 22:32:29 +0300

    added styling dependencies

--- client/src/index.js
+++ client/src/index.js
@@ -1,4 +1,5 @@
 import React from "react";
+import 'bootstrap/dist/css/bootstrap.min.css'; //adding bootstrap css
 import App from "./components/App";
 import "./index.css";
 import ReactDOM from "react-dom";
