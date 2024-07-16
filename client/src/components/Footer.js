4a0b8e0ba50bc8952d7d6ab2b4881fe0ab59b3a6
Kamau-sam <skamau.6411@gmail.com>
2024-07-16 18:08:04 +0300

    "Updated Footer.js: added gif file

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -1,13 +1,17 @@
 import React from "react";
-import 'bootstrap/dist/css/bootstrap.min.css';
-import '@fortawesome/fontawesome-free/css/all.min.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+import "@fortawesome/fontawesome-free/css/all.min.css";
 import "../css/footer.css";
 
 function Footer() {
   return (
     <footer className="footer bg-dark text-white p-3">
       <div className="container d-flex justify-content-between align-items-center">
-        <img src="https://i.pinimg.com/564x/86/4a/3e/864a3e877dc16143e216b145da06a336.jpg" alt="pic" className="footer-logo" />
+        <img
+          src="https://i.pinimg.com/564x/86/4a/3e/864a3e877dc16143e216b145da06a336.jpg"
+          alt="pic"
+          className="footer-logo"
+        />
         <div className="company-footer text-center">
           <h2>FREIGHTX</h2>
           <p>4653-Kilindini</p>
@@ -16,9 +20,15 @@ function Footer() {
         <div className="contacts text-right">
           <h2>Contact us on:</h2>
           <ul className="list-unstyled">
-            <li className="contact-item"><i className="fab fa-twitter"></i> Twitter: @freightx</li>
-            <li className="contact-item"><i className="fab fa-instagram"></i> Instagram: @freightx</li>
-            <li className="contact-item"><i className="fab fa-facebook"></i> Facebook: @freightx</li>
+            <li className="contact-item">
+              <i className="fab fa-twitter"></i> Twitter: @freightx
+            </li>
+            <li className="contact-item">
+              <i className="fab fa-instagram"></i> Instagram: @freightx
+            </li>
+            <li className="contact-item">
+              <i className="fab fa-facebook"></i> Facebook: @freightx
+            </li>
           </ul>
         </div>
       </div>
